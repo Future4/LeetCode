8369d3d5e92d690206531a8ec80d29b2837302ff
Future4 <38273351+Future4@users.noreply.github.com>
2020-11-09 23:11:42 +0800

    Create 回溯算法.js

---
+++ 回溯算法.js
@@ -0,0 +1,24 @@
+ //方法一  递归
+ const arr = [1, 2, 3]
+
+        function params(arr) {
+            let res = []
+            var  dfs= (path)=> {
+                if (path.length == arr.length) {
+                    res.push([...path])
+                    return
+                }
+                for (let i = 1; i <= arr.length; i++) {
+                    if (path.includes(i)) {
+                        continue
+                    }
+                    path.push(i)
+                    dfs(path)
+                    path.pop()
+                }
+            }
+            dfs([])
+            console.log(res)
+            return res
+        }
+        params(arr)
