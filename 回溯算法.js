8862cc0792fa1bf11853eadea7481b4e227fd45b
jian.zhang <jian.zhang@convertlab.com>
2020-12-16 20:30:52 +0800

    排序

--- 回溯算法.js
+++ 回溯算法.js
@@ -1,24 +1,24 @@
- //方法一  递归
- const arr = [1, 2, 3]
+//方法一  递归
+const arr = [1, 2, 3]
 
-        function params(arr) {
-            let res = []
-            var  dfs= (path)=> {
-                if (path.length == arr.length) {
-                    res.push([...path])
-                    return
-                }
-                for (let i = 1; i <= arr.length; i++) {
-                    if (path.includes(i)) {
-                        continue
-                    }
-                    path.push(i)
-                    dfs(path)
-                    path.pop()
-                }
-            }
-            dfs([])
-            console.log(res)
-            return res
+function params(arr) {
+    let res = []
+    var dfs = (path) => {
+        if (path.length == arr.length) {
+            res.push([...path])
+            return
         }
-        params(arr)
+        for (let i = 1; i <= arr.length; i++) {
+            if (path.includes(i)) {
+                continue
+            }
+            path.push(i)
+            dfs(path)
+            path.pop()
+        }
+    }
+    dfs([])
+    console.log(res)
+    return res
+}
+params(arr)
